73842037b47c3f8016929fbd105f340d769418bd
timjolson <>
2019-10-12 16:52:16 -0500

    Remove trueskill and zstd from setup.py dependencies

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 # setup_tools install info
 setup(
     name='halite',
-    version='0.5',
+    version='0.7',
     description='halite match manager',
     author='Tim Olson',
-    author_email='timjolson@user.noreplay.github.com',
+    url='https://gitlab.com/timjolson/halite3-match-manager',
     packages=find_packages(),
-    install_requires=['six>=1.11.0', 'trueskill==0.4.4', 'zstd==1.3.4.4', 'tqdm', 'skills'],
+    install_requires=['six>=1.11.0', 'tqdm', 'skills'],
     python_requires='>3.0',
 )
